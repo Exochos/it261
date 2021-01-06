ca8a02baa7c1359dbca5a2ab3a245c722d93d39e
exochos <exochos@gmail.com>
2021-01-06 03:35:19 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<!doctype html>
+<html lang="en" ng-app="">
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <!-- Viewport so we can change depending on device size -->
+    <meta name='viewport' content='width=device-width,initial-scale=1,user-scalable=no'>
+    <!-- Style sheet lives here -->
+    <link rel="stylesheet" href="css/style.css"> 
+    <!-- javascript -->
+    <script src="project.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="jquery.connections.js"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
+</head>
+
+<body style="width:100%">
+    <!--Flexbox container-->
+    <div class='container'>
+        <div class='left'></div>
+        <div class='middle'>
+        <span style="height:40%"></span>
+	<p>Info PHP is here with error reporting set to on,
+		<a href="http://35.226.47.22/it261/homework/info.php">http://35.226.47.22/it261/homework/info.php</a><br>
+	Test.php is here so you can see that error reporting is on:
+		<a href="http://35.226.47.22/it261/homework/test.php">http://35.226.47.22/it261/homework/test.php</a>
+	</p>
+        <span style="height:50%"></span>
+            <footer style="font-family: 'Audiowide', cursive;">Jeremy Ward	&copy;2021 </footer>
+        </div>
+        <div class='right'></div>
+    </div>
+</body>
