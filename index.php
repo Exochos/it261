a382749b1b890f9ec667f879eefdd5e9cecc56aa
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-18 13:46:36 -0800

    Update index.php
    1

--- index.php
+++ index.php
@@ -19,23 +19,63 @@
 <body style="width:100%">
     <!--Flexbox container-->
     <div class='container'>
-        <div class='left'> </div>
-        <div class='middle'>
-	<?php 
-    	include "php/header.php"; head();
-	?>
-	<p>Info PHP is here with error reporting set to on,
-		<a href="http://35.226.47.22/it261/homework/info.php">http://35.226.47.22/it261/homework/info.php</a><br>
-	<img src="images/error.png" width="50%"><br>
-	Test.php is here so you can see that error reporting is on:
-	<a href="http://35.226.47.22/it261/homework/test.php">http://35.226.47.22/it261/homework/test.php</a><br>
-	<img src="images/test.png" width="50%">
-	</p>
-	<span style="height:30%"></span>
+        <div class='left'> </div><!-- left block -->
+        <div class='middle' style="flex-direction: column;"><!-- Middle is going to hold everything -->
+            <header>
+                <h1 style="font-family: 'Audiowide', cursive;">Jeremy's <sub>Designs</sub></h1>
+                <nav style="text-align: right;font-family: 'Audiowide', cursive;margin-bottom: 1em;" class='nav'>
+                    <a link href="#index.html">Home</a>
+                    <a link href="">Other</a>
+                    <a link href="">More</a>
+                </nav>
+            </header>
+            
+            <main style="height:80%; width:100%;flex-direction: row;">
+                <img src="images/Jeremy-Profile.jpg" style="margin:2em;float: right;height: 20%; border-radius: 50%;">
+                <div style="flex-direction: row; margin: 1em;padding:1em;width:65%;background-color: azure;border-radius: 15%;">
+                    Hi! My name is Jeremy and here are some things about me:<br>
+                    I grew up in California and moved up to the greater Seattle area when I was 9 and have lived here ever since.
+                    I initially went to College at Shoreline CC back in 2000 for Computer Science but dropped out cause my mother moved and never got back into it.
+                    Its only been in the last couple of years that I have had the fortitude to get myself back into Computer Science. Now that I'm back into it however I
+                    am very much enjoying myself with HTML javascript, python, java, and all the rest of the stuff, and now hopefully PHP. :)<br>
+                    I should be getting myself a puppy soon if all goes well and hoping that covid / coups are over soon.
+            </div>
+                <span style="height:2em;">&nbsp;</span>
+                <div style="flex-direction: column-reverse;border-style: solid;border-color:cornflowerblue;border-radius: 25%;width:40%;">
+                <UL style="margin-left: 2em;">
+                    <h2>List of Assignments:</h2>
+                    <li><a href="">Week 2 Class Exercises - Jan 18th</a></li>
+                    <li>
+                        <a href="">MAMP - HW1 - Jan 18th</a>
+                    </li>
+                    <li>
+                        Portal Page - HW2 - Jan 18th
+                    </li>                    
+                    <li>
+                        PHP Fundamentals - Jan 25th
+                    </li>
 
-        <footer style="font-family: 'Audiowide', cursive;">Jeremy Ward	&copy;2021 </footer>
+                    <li>
+                        Using a Switch - HW3 - Jan 25th    
+                    </li>
+                </UL><br>
+            </div>
+                <aside style="height: 20%;">
+                    <h2>Weekly class exercise code list</h2>
+                    <li>
+                        Week 3 Class Exercises - Jan 25th
+                    </li>
+                </aside>
+            </main>
+
+            <span style="height:20%"></span>
+            <footer style="font-family: 'Audiowide', cursive;">
+                Jeremy Ward	&copy; 2021<br>
+                <a href="http://validator.w3.org/check/referer" rel="nofollow" title="Validate as HTML5">HTML5</a>
+                <a href="http://jigsaw.w3.org/css-validator/validator?uri=<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>" rel="nofollow" title="Validate as CSS3">CSS3</a>
+             </footer>
         </div>
-        <div class='right'></div>
+
+        <div class='right'></div> <!-- right block-->
     </div>
 </body>
-</html>
